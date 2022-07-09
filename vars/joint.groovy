26f2007adb9a713d397194573537262c5494748d
ogensogens <105896389+ogensogens@users.noreply.github.com>
2022-07-09 17:44:32 -0400

    upload file

---
+++ vars/joint.groovy
@@ -0,0 +1,19 @@
+def call(String mavenObj){
+  
+  if ("${mavenObj}" == "Clean")
+     {
+       sh "mvn clean"
+     }
+  else if ("${mavenObj}" == "Compile")
+     {
+       sh "mvn clean compile"
+     }
+  else if ("${mavenObj}" == "Test")
+     {
+       sh "mvn clean test"
+     }
+   else if ("${mavenObj}" == "Package")
+      {
+        sh "mvn clean package"
+     }
+}
